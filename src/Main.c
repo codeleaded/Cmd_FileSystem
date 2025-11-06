92bdf9c433496ec75a16913d69fde185d7e7bc34
C-Reaper <gnu.c.reaper@gmail.com>
2025-11-06 18:18:31 +0100

    update

--- src/Main.c
+++ src/Main.c
@@ -1,4 +1,14 @@
-#include "C:/Wichtig/System/Static/Library/FSystem.h"
+#if defined __linux__
+#include "/home/codeleaded/System/Static/Library/FSystem.h"
+#elif defined _WINE
+#include "/home/codeleaded/System/Static/Library/FSystem.h"
+#elif defined _WIN32
+#include "F:/home/codeleaded/System/Static/Library/FSystem.h"
+#elif defined(__APPLE__)
+#error "Apple not supported!"
+#else
+#error "Platform not supported!"
+#endif
 
 int main(int argc,const char *argv[]){
     
@@ -12,14 +22,14 @@ int main(int argc,const char *argv[]){
     FSystem_CreateFile(&fs,"Main/Hello2");
     FSystem_CreateFile(&fs,"Main/Hello3");
 
-    FSystem_PrintTree(&fs);
+    FSystem_Print(&fs);
 
     FSystem_DeleteFile(&fs,"Main/Hello");
     FSystem_DeleteFile(&fs,"Main/Hello3");
     FSystem_DeleteFile(&fs,"Main/Hello0/File1");
     FSystem_DeleteFile(&fs,"Main/Hello1");
 
-    FSystem_PrintTree(&fs);
+    FSystem_Print(&fs);
 
     FSystem_Free(&fs);
     printf("Deleted All!\n");
